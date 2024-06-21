bedb540e4bec353c74ac0b5c15d28ac438f9b8d0
Douglas Amorim <amorimdos@gmail.com>
2024-06-21 19:14:45 -0300

    chore init project

---
+++ MeliProject/MeliProject/MeliProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliProjectApp.swift
+//  MeliProject
+//
+//  Created by Douglas Amorim on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MeliProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
