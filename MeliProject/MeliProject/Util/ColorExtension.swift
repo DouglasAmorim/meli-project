7f9888486f932220a6698ad2c5f1f3261c93174e
Douglas Amorim <amorimdos@gmail.com>
2024-06-22 16:21:16 -0300

    feat implemented search methods

---
+++ MeliProject/MeliProject/Util/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  MeliProject
+//
+//  Created by Douglas Amorim on 22/06/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGrayLight = Color("LightGray")
+}
--- MeliProject/MeliProject/View/DashboardView.swift
+++ MeliProject/MeliProject/View/DashboardView.swift
@@ -11,26 +11,46 @@ struct DashboardView: View {
     @ObservedObject var dashboardViewModel = DashboardViewModel(listProductsService: ProductsService())
     @State private var isShowingFilter: Bool = false
     @AppStorage("selectedCategory") var selectedCategory: String = "MLA1055"
+    @State var searchInput: String = ""
     
     var body: some View {
         NavigationView {
-            List {
-                switch dashboardViewModel.loadingState {
-                case .FAILURE:
-                    Text("Error: \(self.dashboardViewModel.error?.description ?? " Unknown")")
+            VStack(spacing: 12) {
+                HStack(spacing: 0) {
+                    Image(systemName: "magnifyingglass")
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 4)
                     
-                case .LOADING:
-                    Text("Loading")
-                    
-                case .LOADED:
-                    ForEach(dashboardViewModel.products) { product in
-                        NavigationLink(destination: ProductDetailView(product: product), label: {
-                            ProductRowView(product: product)
-                                .padding(.vertical, 4)
-                        })
+                    TextField("Search", text: $searchInput)
+                        .onSubmit {
+                            print("submit \(searchInput)")
+                            self.dashboardViewModel.searchAttributes.search = searchInput
+                            self.dashboardViewModel.getListProducts()
+                        }
+                }
+                .background(Color.customGrayLight)
+                .cornerRadius(16)
+                .padding(.horizontal, 24)
+                
+                List {
+                    switch dashboardViewModel.loadingState {
+                    case .FAILURE:
+                        Text("Error: \(self.dashboardViewModel.error?.description ?? " Unknown")")
+                        
+                    case .LOADING:
+                        Text("Loading")
+                        
+                    case .LOADED:
+                        ForEach(dashboardViewModel.products) { product in
+                            NavigationLink(destination: ProductDetailView(product: product), label: {
+                                ProductRowView(product: product)
+                                    .padding(.vertical, 4)
+                            })
+                        }
                     }
                 }
             }
+
             .navigationTitle("Products")
             .navigationBarItems(trailing:
                                     Button(action: {
